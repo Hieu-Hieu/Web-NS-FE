c130f754beb752339c4abcc100b842ae45c97412
hieuhieu <coffeenguyen1611@gmail.com>
2023-02-26 15:40:43 +0700

    add fb chat

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,98 +1,108 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import GoogleLogin from 'react-google-login';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import GoogleLogin from "react-google-login";
 
-import Helmet from '../components/Helmet'
-import { userLoginAction } from '../redux/actions/userAction'
+import Helmet from "../components/Helmet";
+import { userLoginAction } from "../redux/actions/userAction";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error1, setError1] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error1, setError1] = useState("");
 
-    const { search } = useLocation();
-    const dispatch = useDispatch();
-    //get redirect params
-    const redirectURL = new URLSearchParams(search).get('redirect');
-    const redirect = redirectURL ? redirectURL : '/';
-    let navigate = useNavigate();
+  const { search } = useLocation();
+  const dispatch = useDispatch();
+  //get redirect params
+  const redirectURL = new URLSearchParams(search).get("redirect");
+  const redirect = redirectURL ? redirectURL : "/";
+  let navigate = useNavigate();
 
-    const userSignin = useSelector(state => state.userSignin);
-    const { userInfo, loading, error } = userSignin;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+  console.log(userSignin);
 
-    useEffect(() => {
-
-        if (userInfo) {
-            if (userInfo.role === 'admin') {
-                navigate('/admin')
-            } else {
-                navigate(redirect)
-            }
-        }
-
-    }, [redirect, userInfo]);
-
-    useEffect(() => {
-        console.log('err')
-        if (error && !error1) {
-            setError1(error);
-            setTimeout(() => {
-                setError1('');
-            }, 3000)
-        }
-    }, [error])
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(userLoginAction({ email, password }));
+  useEffect(() => {
+    if (userInfo) {
+      if (userInfo.role === "admin") {
+        navigate("/admin");
+      } else {
+        navigate(redirect);
+      }
     }
+  }, [redirect, userInfo]);
 
-    const handleGGLogin = (googleData) => {
-        dispatch(userLoginAction({ token: googleData.tokenId, ggId: googleData.googleId }));
+  useEffect(() => {
+    if (error && !error1) {
+      setError1(error);
+      setTimeout(() => {
+        setError1("");
+      }, 3000);
     }
+  }, [error]);
 
-    const handleGGFailure = () => {
-        console.log("Google Login Fail");
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(userLoginAction({ email, password }));
+  };
 
-    return (
-        <Helmet title="Đăng nhập">
-            <div className="login">
-                <form onSubmit={handleSubmit} >
-                    <h3>Đăng Nhập</h3>
-                    {loading && <div>Đăng nhập...</div>}
-                    {error1 && <div className="form-alert">{error1}</div>}
-                    <div className="input-group">
-                        <input type="email" placeholder="Địa chỉ email" required
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <i className="far fa-envelope fa-2x i-login"></i>
-                    </div>
-                    <div className="input-group">
-                        <input type="password" placeholder="Mật khẩu" required
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <i className="fas fa-lock fa-2x i-login"></i>
-                        <box-icon name='show'></box-icon>
-                    </div>
-                    <button className="btn btn-submit" type="submit">
-                        Đăng nhập</button>
-                    <p>Quên mật khẩu ? <Link to="/get-token">Nhấn vào đây</Link></p>
-                    <p>Bạn chưa có tài khoản chưa?<Link to="/register">Đăng ký</Link> </p>
-                    <p>Hoặc</p>
-                    <GoogleLogin
-                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                        buttonText="Login with Google"
-                        onSuccess={handleGGLogin}
-                        onFailure={handleGGFailure}
-                        cookiePolicy={'single_host_origin'}
-                    ></GoogleLogin>
-                </form>
-            </div>
-        </Helmet>
+  const handleGGLogin = (googleData) => {
+    dispatch(
+      userLoginAction({ token: googleData.tokenId, ggId: googleData.googleId })
+    );
+  };
 
-    )
-}
+  const handleGGFailure = () => {
+    console.log("Google Login Fail");
+  };
+
+  return (
+    <Helmet title="Đăng nhập">
+      <div className="login">
+        <form onSubmit={handleSubmit}>
+          <h3>Đăng Nhập</h3>
+          {loading && <div>Đăng nhập...</div>}
+          {error1 && <div className="form-alert">{error1}</div>}
+          <div className="input-group">
+            <input
+              type="email"
+              placeholder="Địa chỉ email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <i className="far fa-envelope fa-2x i-login"></i>
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              placeholder="Mật khẩu"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i className="fas fa-lock fa-2x i-login"></i>
+            <box-icon name="show"></box-icon>
+          </div>
+          <button className="btn btn-submit" type="submit">
+            Đăng nhập
+          </button>
+          <p>
+            Quên mật khẩu ? <Link to="/get-token">Nhấn vào đây</Link>
+          </p>
+          <p>
+            Bạn chưa có tài khoản chưa?<Link to="/register">Đăng ký</Link>{" "}
+          </p>
+          <p>Hoặc</p>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Login with Google"
+            onSuccess={handleGGLogin}
+            onFailure={handleGGFailure}
+            cookiePolicy={"single_host_origin"}
+          ></GoogleLogin>
+        </form>
+      </div>
+    </Helmet>
+  );
+};
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,87 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import Helmet from '../components/Helmet'
-import { Link } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import Helmet from "../components/Helmet";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { register } from '../redux/actions/userAction'
+import { register } from "../redux/actions/userAction";
 
 const Register = (props) => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setconfirmPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
+  const redirect = props?.location?.search
+    ? props.location.search?.split("=")[1]
+    : "/";
 
-    const redirect = props.location.search
-        ? props.location.search.split('=')[1]
-        : '/';
+  const userRegister = useSelector((state) => state.userRegister);
+  const { userInfo, loading, error } = userRegister;
+  const dispatch = useDispatch();
 
-    const userRegister = useSelector(state => state.userRegister);
-    const { userInfo, loading, error } = userRegister;
-    const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Mật khẩu bạn nhập lại không khớp!");
+    } else {
+      dispatch(register({ firstName, lastName, email, password }));
+    }
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if (password !== confirmPassword) {
-            alert('Mật khẩu bạn nhập lại không khớp!');
-        } else {
-            dispatch(register({ firstName, lastName, email, password }));
-        }
-    };
+  useEffect(() => {
+    if (userInfo) {
+      props?.history?.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
 
-    useEffect(() => {
-        if (userInfo) {
-            props.history.push(redirect);
-        }
-    }, [props.history, redirect, userInfo])
+  return (
+    <Helmet title="Đăng ký tài khoản">
+      <div className="login">
+        <form action="" onSubmit={submitHandler}>
+          <h3>Đăng kí tài khoản</h3>
+          {loading && <div>Loading...</div>}
+          {error && <div>{error}</div>}
+          <div className="input-group">
+            <label htmlFor="">Họ và tên đệm</label>
+            <input
+              type="text"
+              placeholder="Nguyễn Văn"
+              required
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="">Tên</label>
+            <input
+              type="text"
+              placeholder="Hiếu"
+              required
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="">Địa chỉ email</label>
+            <input
+              type="text"
+              placeholder="nva@gmail.com"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <i className="far fa-envelope fa-2x"></i>
+          </div>
+          <div className="input-group">
+            <label htmlFor="">Mật khẩu</label>
+            <input
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i className="fas fa-lock fa-2x"></i>
+          </div>
+          <div className="input-group">
+            <label htmlFor="">Nhập lại mật khẩu</label>
+            <input
+              type="password"
+              required
+              onChange={(e) => setconfirmPassword(e.target.value)}
+            />
+            <i className="fas fa-lock fa-2x"></i>
+          </div>
+          <button className="btn btn-submit" type="submit">
+            Đăng kí
+          </button>
+          <p>
+            Qay lại trang đăng nhập? <Link to="/login">Đăng nhập</Link>
+          </p>
+        </form>
+      </div>
+    </Helmet>
+  );
+};
 
-    return (
-        <Helmet title="Đăng ký tài khoản">
-            <div className="login">
-                <form action="" onSubmit={submitHandler}>
-                    <h3>Đăng kí tài khoản</h3>
-                    {loading && <div>Loading...</div>}
-                    {error && <div>{error}</div>}
-                    <div className="input-group">
-                        <label htmlFor="">Họ và tên đệm</label>
-                        <input type="text" placeholder="Nguyễn Văn" required
-                            onChange={e => setLastName(e.target.value)}
-                        />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="">Tên</label>
-                        <input type="text" placeholder="Hiếu" required
-                            onChange={e => setFirstName(e.target.value)}
-                        />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="">Địa chỉ email</label>
-                        <input type="text" placeholder="nva@gmail.com" required
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <i className="far fa-envelope fa-2x"></i>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="">Mật khẩu</label>
-                        <input type="password" required
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <i className="fas fa-lock fa-2x"></i>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="">Nhập lại mật khẩu</label>
-                        <input type="password" required
-                            onChange={e => setconfirmPassword(e.target.value)}
-                        />
-                        <i className="fas fa-lock fa-2x"></i>
-                    </div>
-                    <button className="btn btn-submit" type="submit">Đăng kí</button>
-                    <p>Qay lại trang đăng nhập? <Link to="/login">Đăng nhập</Link></p>
-                </form>
-            </div>
-        </Helmet>
-    )
-}
-
-export default Register
+export default Register;
